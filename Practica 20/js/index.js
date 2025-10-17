4a00e8c5d11dd7df036a7540a5aa787f0c8e4141
august-luna <cesarlufo@gmail.com>
2025-10-17 07:46:50 -0600

    Practica 20

---
+++ Practica 20/js/index.js
@@ -0,0 +1,26 @@
+let nombre = "César Augusto";
+let apellido = 'Luna Fonseca';
+let nombreCompleto = nombre + " " + apellido;
+
+console.log(nombreCompleto);
+
+
+let numero = 250;
+let numero2 = 300;
+var numero3 = 100;
+numero4 = 50; //declaraciones antiguas
+
+console.log(numero, numero3);
+
+
+let suma = numero + numero2 + numero3 + numero4;
+console.log(suma);
+
+let resta = numero2 - numero3;
+console.log('El resultado es: ', resta);
+
+let multiplica = (numero * numero2);
+console.log('El resultado es: ', multiplica);
+
+let divide = (numero3 / numero4);
+console.log('El resultado es: ', divide);
